934a6c0969d584611f41b94341246ae827b025d6
Guus Hoekman <mail@guushoekman.com>
2017-12-13 09:45:42 +0200

    analytics, preview banner

--- js/action-takers.js
+++ js/action-takers.js
@@ -1,21 +1,28 @@
+var url = window.location.href;
+
 $(".page-share .facebook").on("click", function(e) {
   e.preventDefault();
-  window.open("https://www.facebook.com/sharer/sharer.php?u="+window.location.href,
-    "share", "width=600, height=400, scrollbars=no");
+  window.open("https://www.facebook.com/sharer/sharer.php?u="+window.location.href, "share", "width=600, height=400, scrollbars=no");
   ga('send', 'social', 'facebook', 'share', url);
 });
 
 $(".page-share .twitter").on("click", function(e) {
   e.preventDefault();
-  window.open("https://twitter.com/intent/tweet?&url="+window.location.href,
-    "share", "width=600, height=400, scrollbars=no");
+  window.open("https://twitter.com/intent/tweet?&url="+window.location.href, "share", "width=600, height=400, scrollbars=no");
   ga('send', 'social', 'twitter', 'share', url);
 });
 
 $(".page-share .email").on("click", function(e) {
   e.preventDefault();
-  var url = window.location.href;
   var emailTitle = encodeURIComponent(document.title).replace(/&/g, '%26');
   window.location = 'mailto:?subject=' + emailTitle + '&body=I saw this on Action Takers and thought you might be interested: ' + url;
   ga('send', 'social', 'email', 'share', url);
+});
+
+$(".guide-meta .download").click(function() {
+  ga('send', 'event', 'pdf', 'download', url);
+});
+
+$(".panel-heading.title-box > div").click(function() {
+  ga('send', 'event', 'step toggle', $(".circle",this).text() + ". " + $(".title",this).text(), url );
 });
